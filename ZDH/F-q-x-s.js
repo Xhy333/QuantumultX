6cc288446089d82ff2bcf7cb613230df5e2a5fa7
Xhy333 <101529926+Xhy333@users.noreply.github.com>
2025-06-12 03:25:32 +0800

    更新 F-q-x-s.js

--- ZDH/F-q-x-s.js
+++ ZDH/F-q-x-s.js
@@ -1,6 +1,6 @@
 /* 名称: 番茄小说每日签到
 // 作者: 
-// 描述: 自动完成番茄小说每日签到任务，领取番茄奖励 浏览器访问: https://fanqienovel.com/登录番茄小说账号在Quantumult X的日志中找到`fanqienovel.com`的请求复制`Cookie`字段的全部内容
+// 描述: 自动完成番茄小说每日签到任务，领取番茄
 // 支持: Quantumult X 
 // 远程链接: https://raw.githubusercontent.com/Xhy333/QuantumultX/refs/heads/main/ZDH/F-q-x-s.js
 
