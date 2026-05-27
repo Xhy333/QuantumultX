f953522c9d2865e6e2cd69e50745c8fa02096451
Xhy333 <101529926+Xhy333@users.noreply.github.com>
2026-05-27 15:31:51 +0800

    创建 Kkjp.js

---
+++ ZDH/Kkjp.js
@@ -0,0 +1,8 @@
+#kk键盘
+解锁无限变声功能
+
+^https?:\/\/kk\.weshine\.im\/v1\.0\/text2voice\/(checkCount|consumeCount) url jsonjq-response-body .data.totalCount = 999 | .data.currCount = 999
+
+^https?:\/\/kk\.weshine\.im\/v1\.0\/text2voice\/createTtsAudio url jsonjq-response-body .data.freeCount = 999
+
+hostname = kk.weshine.im
