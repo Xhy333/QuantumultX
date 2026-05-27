859626c671e0181a058259d3eec1a559c2339bb5
Xhy333 <101529926+Xhy333@users.noreply.github.com>
2026-05-27 15:33:55 +0800

    更新 Kkjp.js

--- ZDH/Kkjp.js
+++ ZDH/Kkjp.js
@@ -1,5 +1,25 @@
-#kk键盘
-解锁无限变声功能
+/**************************
+ *  * @Author:
+ * @LastMod:
+ kk键盘
+ 解锁无限变声功能
+
+仅供学习参考，请于下载后24小时内删除
+
+********************************
+
+
+
+使用方法：
+
+
+单独脚本地址：
+https://raw.githubusercontent.com/Xhy333/QuantumultX/refs/heads/main/ZDH/Kkjp.js
+
+********************************/
+
+
+
 
 ^https?:\/\/kk\.weshine\.im\/v1\.0\/text2voice\/(checkCount|consumeCount) url jsonjq-response-body .data.totalCount = 999 | .data.currCount = 999
 
